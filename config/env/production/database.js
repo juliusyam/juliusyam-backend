91aee312f60009b9a274dcbcf23c2b27ae84a419
juliusyam <juliusworldwide@gmail.com>
2022-10-02 00:27:16 +0100

    fixed import issue with database.js

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,4 +1,4 @@
-import parse = require('pg-connection-string').parse;
+const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
 
 export default ({ env }) => ({
