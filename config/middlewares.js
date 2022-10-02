a2708a77e97e050de77467a1a167592899e16791
juliusyam <juliusworldwide@gmail.com>
2022-10-02 11:43:10 +0100

    updated strapi to store files in aws s3

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,3 +1,7 @@
+
+const bucketName = process.env.AWS_BUCKET;
+const region = process.env.AWS_REGION;
+
 module.exports = [
   'strapi::errors',
   'strapi::security',
@@ -9,4 +13,30 @@ module.exports = [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${ bucketName }.s3.${ region }.amazonaws.com`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `${ bucketName }.s3.${ region }.amazonaws.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
