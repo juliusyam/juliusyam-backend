fae0744886d2afa0c3c96fffb2860b7b3c476480
juliusyam <juliusworldwide@gmail.com>
2022-10-02 01:28:52 +0100

    remove unused mock entry

--- src/api/mock/controllers/mock.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * mock controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::mock.mock');
--- src/api/mock/services/mock.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * mock service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::mock.mock');
