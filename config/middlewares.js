a7c5bcc4e59e4416380b8a7ce914b23507b8fe27
juliusyam <juliusworldwide@gmail.com>
2022-10-27 19:52:57 +0100

    updated CORS policy

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,8 +5,15 @@ const bucketUrl = `https://${ bucketName }.s3.amazonaws.com`;
 
 module.exports = [
   'strapi::errors',
-  'strapi::cors',
   'strapi::poweredBy',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:1337', 'http://localhost:3000', 'https://resonant-cajeta-d298bf.netlify.app/']
+    }
+  },
   'strapi::logger',
   'strapi::query',
   'strapi::body',
